dc8c6197bd3da55626502fc5772654b0910022f7
Henrique César <henrique.jose@zup.com.br>
2021-10-06 16:22:56 -0300

    adição de validator de email unico no UsuarioController

--- src/main/java/br/com/zupacademy/henriquecesar/mercadolivre/controller/UsuarioController.java
+++ src/main/java/br/com/zupacademy/henriquecesar/mercadolivre/controller/UsuarioController.java
@@ -4,12 +4,15 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.zupacademy.henriquecesar.mercadolivre.form.UsuarioForm;
+import br.com.zupacademy.henriquecesar.mercadolivre.form.validator.EmailUnicoValidator;
 import br.com.zupacademy.henriquecesar.mercadolivre.modelo.Usuario;
 import br.com.zupacademy.henriquecesar.mercadolivre.repository.UsuarioRepository;
 
@@ -22,6 +25,14 @@ public class UsuarioController {
     
     @Autowired
     private PasswordEncoder passwordEncoder;
+    
+    @Autowired
+    private EmailUnicoValidator emailUnicoValidator;
+    
+    @InitBinder
+    protected void initBinder(WebDataBinder binder) {
+        binder.setValidator(emailUnicoValidator);
+    }
     
     @PostMapping
     public void cadastraUsuario(@Valid @RequestBody UsuarioForm form) {
