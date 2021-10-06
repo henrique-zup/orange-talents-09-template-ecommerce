4b6348ca4fdcb4d083ffc23008ede75ea15c0baf
Henrique César <henrique.jose@zup.com.br>
2021-10-06 16:20:42 -0300

    adição de método para buscar usuário por login

--- src/main/java/br/com/zupacademy/henriquecesar/mercadolivre/repository/UsuarioRepository.java
+++ src/main/java/br/com/zupacademy/henriquecesar/mercadolivre/repository/UsuarioRepository.java
@@ -1,9 +1,13 @@
 package br.com.zupacademy.henriquecesar.mercadolivre.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import br.com.zupacademy.henriquecesar.mercadolivre.modelo.Usuario;
 
 public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
+
+    Optional<Usuario> findUsuarioByLogin(String string);
 
 }
