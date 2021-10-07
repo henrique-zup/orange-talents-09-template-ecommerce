7ddb63c5987ee8f73e6af3d2a6d6949a56d964e7
Henrique César <henrique.jose@zup.com.br>
2021-10-07 08:09:39 -0300

    criação da interface CategoriaRepository

---
+++ src/main/java/br/com/zupacademy/henriquecesar/mercadolivre/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package br.com.zupacademy.henriquecesar.mercadolivre.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.zupacademy.henriquecesar.mercadolivre.modelo.Categoria;
+
+public interface CategoriaRepository extends CrudRepository<Categoria, Long> {
+
+}
