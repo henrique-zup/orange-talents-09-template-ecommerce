2453e1fb3adbb4917f948d632647c1e51075b220
Henrique César <henrique.jose@zup.com.br>
2021-10-08 21:34:59 -0300

    criação da entidade ImagemProduto

---
+++ src/main/java/br/com/zupacademy/henriquecesar/mercadolivre/modelo/ImagemProduto.java
@@ -0,0 +1,33 @@
+package br.com.zupacademy.henriquecesar.mercadolivre.modelo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+
+
+@Entity
+public class ImagemProduto {
+    
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @NotBlank @Column(nullable = false)
+    private String link;
+    
+    @ManyToOne
+    private Produto produto;
+
+    @Deprecated
+    public ImagemProduto() {
+    }
+
+    public ImagemProduto(@NotBlank String link, Produto produtos) {
+        this.link = link;
+        this.produto = produtos;
+    }
+
+}
